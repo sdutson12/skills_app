5211fb03e6e0362971567d62a0b66450dc62004e
Sam Dutson <dutson.samuel@gmail.com>
2019-07-16 12:19:46 -0300

    Finished crash testing for models

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,26 @@
 class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def create
+    @skill = Skill.find(params[:skill_id])
+    @booking = Booking.new(booking_params)
+    @skill = @booking.skill
+    if @booking.save
+      redirect_to skill_path(@skill)
+    else
+      render "skills/show"
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:status, :start_date, :end_date, :skill_id, :user_id)
+  end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,8 +1,9 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :skill
+  has_many :reviews, dependent: :destroy
 
-  validates :start_date, presence: true
-  validates :end_date, presence: true
-  validates :status, presence: true, default: "pending"
+  # validates :start_date, presence: true
+  # validates :end_date, presence: true
+  # validates :status, presence: true
 end
