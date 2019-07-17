ad1e7a2807489fe8d618bda2b010f57b90425680
Lisa Chinn <lisamchinn@gmail.com>
2019-07-17 11:53:31 -0300

    finished seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_16_175644) do
+ActiveRecord::Schema.define(version: 2019_07_17_140838) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookings", force: :cascade do |t|
     t.string "status"
-    t.date "start_date"
+    t.date "date"
     t.date "end_date"
     t.bigint "skill_id"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "num_of_hours"
     t.index ["skill_id"], name: "index_bookings_on_skill_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
@@ -46,6 +47,7 @@ ActiveRecord::Schema.define(version: 2019_07_16_175644) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "photo"
+    t.string "location"
     t.index ["user_id"], name: "index_skills_on_user_id"
   end
 
