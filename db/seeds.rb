9d2947901997ee0932b8347ed56f2d7b929b1b94
Lisa Chinn <lisamchinn@gmail.com>
2019-07-17 11:07:23 -0300

    completed seeds, pre-test

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,40 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# SEEDS INFO
 
-User.destroy_all
-User.create!(
+LEARNERS = [ {
+  first_name: 'Faviola',
+  last_name: 'Caballero',
+  password: 123456754,
+  email: 'faviola@mail.com',
+  photo: 'https://avatars2.githubusercontent.com/u/50439580?v=4'
+  },
+  {
+  first_name: 'Fer',
+  last_name: 'E. Silva Jacquier',
+  password: 123456,
+  email: 'fer@mail.com',
+  photo: 'https://avatars0.githubusercontent.com/u/47560671?v=4'
+  },
+  {
+  first_name: 'Luis',
+  last_name: 'Zhang',
+  password: 1234567,
+  email: 'luis@mail.com',
+  photo: 'https://avatars1.githubusercontent.com/u/46462316?v=4'
+  },
+  {
+  first_name: 'Pin',
+  last_name: 'Ratnajarn',
+  password: 12345678,
+  email: 'pin@mail.com',
+  photo: 'https://avatars1.githubusercontent.com/u/37698593?v=4'
+  },
+  {
+  first_name: 'Sole',
+  last_name: 'Escobar',
+  password: 123456789,
+  email: 'sole@mail.com',
+  photo: 'https://avatars0.githubusercontent.com/u/48864600?v=4'
+  },
   {
   first_name: 'Alberta',
   last_name: 'Lowe',
@@ -15,49 +42,43 @@ User.create!(
   email: 'alberta@mail.com',
   photo: 'https://avatars0.githubusercontent.com/u/50481949?v=4'
   }
-)
+}]
 
-User.create!(
+INSTRUCTORS = [  {
+  first_name: 'Alberta',
+  last_name: 'Lowe',
+  password: 123456754,
+  email: 'alberta@mail.com',
+  photo: 'https://avatars0.githubusercontent.com/u/50481949?v=4'
+  },
   {
   first_name: 'Lisa',
   last_name: 'Chinn',
   password: 123456,
   email: 'lisa@mail.com',
   photo: 'https://avatars1.githubusercontent.com/u/50211536?v=4'
-  }
-)
-
-User.create!(
+  },
   {
   first_name: 'Sam',
   last_name: 'Dutson',
   password: 1234567,
   email: 'sam@mail.com',
   photo: 'https://avatars3.githubusercontent.com/u/27783841?v=4'
-  }
-)
-
-User.create!(
+  },
   {
   first_name: 'Leo',
   last_name: 'Iamele',
   password: 12345678,
   email: 'leo@mail.com',
   photo: 'https://avatars1.githubusercontent.com/u/39697541?v=4'
-  }
-)
-
-User.create!(
+  },
   {
   first_name: 'Sebas',
   last_name: 'Buffo',
   password: 123456789,
   email: 'sebas@mail.com',
   photo: 'https://avatars0.githubusercontent.com/u/18401014?s=460&v=4'
-  }
-)
-
-User.create!(
+  },
   {
   first_name: 'Samu',
   last_name: 'Levy',
@@ -65,255 +86,239 @@ User.create!(
   email: 'samu@mail.com',
   photo: 'https://avatars0.githubusercontent.com/u/21371991?s=460&v=4'
   }
-)
+]
 
-Skill.destroy_all
-Skill.create!(
-  {
-    title: 'Learn to play hockey with Alberta!' ,
-    description: 'Hi! I\'m Alberta, and I\'ve been playing hockey for 8 years teaching it for 3 years. I would love to share my skills with you! Hope to meet you soon :)',
+
+SKILLS = [ {
+    title: 'Learn to play hockey with me!' ,
+    description: 'Hi! I\'ve been playing hockey for 8 years teaching it for 3 years. I would love to share my skills with you! Hope to meet you soon :)',
     sport: 'hockey',
     price: 30,
-    photo: 'https://avatars0.githubusercontent.com/u/50481949?v=4',
-    user_id: 1
-  }
-)
-
-Skill.create!(
+    location: 'Boston',
+    photo: 'https://images.unsplash.com/photo-1554186285-e2bc44c34216?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80'
+  },
   {
-    title: 'Learn to play golf with Alberta!' ,
-    description: 'Hi! I\'m Alberta, and I\'ve been teaching golf for 7 years. I would love to share my skills with you! Hope to meet you soon :)',
+    title: 'Learn to play golf with me!' ,
+    description: 'Hi! I\'ve been teaching golf for 7 years. I would love to share my skills with you! Hope to meet you soon :)',
     sport: 'golf',
     price: 65,
-    photo: 'https://avatars0.githubusercontent.com/u/50481949?v=4',
-    user_id: 1
-  }
-)
-
-Skill.create!(
+    location: 'Buenos Aires',
+    photo: 'https://images.unsplash.com/photo-1433800881591-a31e7bff3f22?ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80'
+  },
   {
-    title: 'Learn to do yoga with Alberta!',
-    description: 'Hi! I\'m Alberta, and I\'ve been doing yoga my whole life! I would love to share my skills with you! Hope to meet you soon :)',
+    title: 'Learn to do yoga! Beginners welcome!',
+    description: 'Hi! I\'ve been doing yoga my whole life! I would love to share my skills with you! Hope to meet you soon :)',
     sport: 'yoga',
     price: 30,
-    photo: 'https://avatars0.githubusercontent.com/u/50481949?v=4',
-    user_id: 1
-  }
-)
-
-Skill.create!(
+    location: 'New York City',
+    photo: 'https://images.unsplash.com/photo-1473022082832-5a30701d4f2c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80'
+  },
   {
-    title: 'Come play ball with Sam',
-    description: 'Hey there! I\'m Sam, and I\'ve love playing \'ball! I\'ll turn you into the next Uncle Drew, and if you don\'t know who that is, don\'t bother messaging me! :)',
+    title: 'Come play basketball with me',
+    description: 'Hey there! I\'ve love playing \'ball! I\'ll turn you into the next Uncle Drew!',
     sport: 'basketball',
     price: 45,
-    photo: 'https://avatars3.githubusercontent.com/u/27783841?v=4',
-    user_id: 3
-  }
-)
-
-Skill.create!(
+    location: 'Los Angeles',
+    photo: 'https://images.unsplash.com/photo-1525916192735-22142b82d37a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80'
+  },
   {
-    title: 'Climbing lesson with Sam!',
-    description: 'Hola! I\'ve been climbing for a few years, and I\'ve ' ,
+    title: 'Climbing lessons',
+    description: 'Hey there! I\'ve been climbing for a few years, and I\'ve really fallen in love with the sport. I would love to share my passion for climbing with you!',
     sport: 'climbing',
-    price: 75,
-    photo: 'https://avatars3.githubusercontent.com/u/27783841?v=4',
-    user_id: 3
-  }
-)
-
-Skill.create!(
+    price: 70,
+    location: 'Denver',
+    photo: 'https://images.unsplash.com/photo-1522163182402-834f871fd851?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1386&q=80'
+  },
   {
-    title: 'Swimming lessons with Leo!',
+    title: 'Swimming lessons!',
     description: 'I\'m an avid swimmer. Let\'s have fun swimming together!',
     sport: 'swimming',
     price: 35,
-    photo: 'https://avatars1.githubusercontent.com/u/39697541?v=4',
-    user_id: 4
+    location: 'San Diego',
+    photo: 'https://images.unsplash.com/photo-1438029071396-1e831a7fa6d8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2584&q=80'
+    user_id
   }
-)
+}]
 
-# do I need user_id?
-
-Booking.destroy_all
-Booking.create!(
+BOOKINGS = [ {
+    status: 'accepted',
+    date: Date.today,
+    num_of_hours: 1,
+    user_id
+    skill_id
+  },
   {
     status: 'accepted',
-    start_date: Date.today,
-    end_date: Date.today,
-    skill_id: 1,
-    user_id: 2
-  }
-)
-
-Booking.create!(
-  {
-    status: 'pending',
-    start_date: Date.today,
-    end_date: Date.today,
-    skill_id: 4,
-    user_id: 2
-  }
-)
-
-Booking.create!(
+    date: Date.today,
+    num_of_hours: 2,
+  },
   {
     status: 'accepted',
-    start_date: Date.today,
-    end_date: Date.today,
-    skill_id: 6,
-    user_id: 2
-  }
-)
-
-Booking.create!(
+    date: Date.today,
+    num_of_hours: 1.5,
+  },
+  {
+    status: 'accepted',
+    date: Date.today,
+    num_of_hours: 1.5,
+  },
+  {
+    status: 'accepted',
+    date: Date.today,
+    num_of_hours: 2.5,
+  },
+  {
+    status: 'accepted',
+    date: Date.today,
+    num_of_hours: 3,
+  },
   {
     status: 'pending',
-    start_date: Date.today,
-    end_date: Date.tomorrow,
-    skill_id: 3,
-    user_id: 6
-  }
-)
-
-Booking.create!(
+    date: Date.tomorrow,
+    num_of_hours: 2,
+  },
+  {
+    status: 'pending',
+    date: Date.today,
+    num_of_hours: 1,
+  },
   {
     status: 'cancelled',
-    start_date: Date.today,
-    end_date: Date.tomorrow,
-    skill_id: 5,
-    user_id: 6
-  }
-)
-
-Booking.create!(
+    date: Date.tomorrow,
+    num_of_hours: 1.5,
+  },
   {
-    status: 'accepted',
-    start_date: Date.today,
-    end_date: Date.tomorrow,
-    skill_id: 2,
-    user_id: 6
-  }
-)
+    status: 'pending',
+    date: Date.tomorrow,
+    num_of_hours: 1,
+  },
+]
 
-Booking.create!(
-  {
-    status: 'accepted',
-    start_date: Date.today,
-    end_date: Date.tomorrow,
-    skill_id: 1,
-    user_id: 5
-  }
-)
-
-Booking.create!(
-  {
-    status: 'accepted',
-    start_date: Date.today,
-    end_date: Date.tomorrow,
-    skill_id: 2,
-    user_id: 5
-  }
-)
-
-Booking.create!(
-  {
-    status: 'accepted',
-    start_date: Date.today,
-    end_date: Date.tomorrow,
-    skill_id: 3,
-    user_id: 5
-  }
-)
-
-Booking.create!(
-  {
-    status: 'accepted',
-    start_date: Date.today,
-    end_date: Date.tomorrow,
-    skill_id: 4,
-    user_id: 1
-  }
-)
-
-Review.create!(
-  {
-    title: 'Best instructor ever !',
-    content: 'Can\'t believe how much better I am after just one session! Highly recommend!',
-    stars: 5,
-    booking_id: 1
-  }
-)
-Review.create!(
+REVIEWS = [ {
+  title: 'Best instructor ever!',
+  content: 'Can\'t believe how much better I am after just one session! Highly recommend!',
+  stars: 5,
+  },
   {
     title: 'Bestest instructor ever ever!',
     content: 'Can\'t believe how much better I am after just one session! Very much recommend!',
     stars: 4,
-    booking_id: 2
-  }
-)
-Review.create!(
+  },
   {
     title: 'Best best instructor ever!',
     content: 'Can\'t believe how much better I am after just one session! I definitely recommend!',
     stars: 5,
-    booking_id: 3
-  }
-)
-Review.create!(
+  },
   {
     title: 'Most amazing instructor ever!',
     content: 'Can\'t believe how much better I am after just one session! I recommend!',
     stars: 4,
-    booking_id: 4
-  }
-)
-Review.create!(
+  },
   {
     title: 'Wonderful instructor!',
     content: 'Can\'t believe how much better I am after just one session! Most def recommend!',
     stars: 5,
-    booking_id: 5
-  }
-)
-Review.create!(
+  },
   {
     title: 'Great teacher!',
     content: 'Can\'t believe how much better I am after just one session!',
     stars: 4,
-    booking_id: 6
-  }
-)
-Review.create!(
+  },
   {
     title: 'Best teacher ever!',
     content: 'Can\'t believe how much better I am after just one session!!!!',
     stars: 5,
     booking_id: 7
-  }
-)
-Review.create!(
+  },
   {
     title: 'Best maestro ever!',
     content: 'Can\'t believe how much better I am after just one session! Highly recommend!!!',
     stars: 4,
-    booking_id: 8
-  }
-)
-Review.create!(
+  },
   {
     title: 'Awesome teacher!',
     content: 'Can\'t believe how much better I am after just one session! Highly recommend!!!!!!',
     stars: 5,
-    booking_id: 9
-  }
-)
-Review.create!(
+  },
   {
     title: 'Best teacher I have ever had!',
     content: 'Can\'t believe how much better I am after just one session! You should definitely take a lesson!!!!',
     stars: 4,
-    booking_id: 10
   }
-)
+]
+
+# SEEDING PROCESS
+
+puts "Cleaning database"
+User.destroy_all
+Skill.destroy_all
+Booking.destroy_all
+Review.destroy_all
+
+puts ""
+puts "Starting seeding process..."
+
+puts ""
+puts "Creating Learners"
+LEARNERS.each do |learner|
+  seed_learner = User.new(learner)
+  seed_learner.remote_photo_url = learner[:photo]
+  seed_learner.save!
+  puts " - created #{seed_learner.first_name}"
+end
+
+puts ""
+puts "Creating Instructors"
+INSTRUCTORS.each do |instructor|
+  seed_instructor = User.new(instructor)
+  seed_instructor.remote_photo_url = instructor[:photo]
+  seed_instructor.save!
+  puts " - created #{seed_learner.first_name}"
+end
+
+puts ""
+puts "Creating Skills"
+SKILLS.each do |skill|
+  seed_skill = Skill.new(skill)
+  seed_skill.user = Instructor.all.sample
+  seed_skill.remote_photo_url = skill[:photo]
+  seed_skill.save!
+  puts " - created #{seed_skill.title}"
+end
+
+puts ""
+puts "Creating Bookings"
+BOOKING.each do |booking|
+  seed_booking = Booking.new(booking)
+  seed_booking.user = Learner.all.sample
+  seed_booking.skill = Skill.all.sample
+  seed_booking.save!
+  puts " - created #{seed_booking.date}"
+end
+
+puts ""
+puts "Creating Reviews"
+REVIEWS.each do |review|
+  seed_review = Review.new(review)
+  seed_review.booking = Booking.all.sample
+  seed_review.save!
+  puts " - created #{seed_review.title}"
+end
+
+puts "Finished seeding process..."
+
+
+
+
+# url = "http://static.giantbomb.com/uploads/original/9/99864/2419866-nes_console_set.png"
+# article = Article.new(title: 'NES', body: "A great console")
+# article.remote_photo_url = url
+# article.save
+
+
+# INSTRUCTORS.each do |instructor|
+#   if rand(0..1) == 1
+#     seed_skill.user = instructor
+#     seed_skill.save!
+#   end
+# end
