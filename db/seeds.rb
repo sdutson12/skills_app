43a8736881e975a8d807ffb4f65b3fa31442edc4
Lisa Chinn <lisamchinn@gmail.com>
2019-07-18 12:07:24 -0300

    updated seeds with user information and some other things, added some more reviews + bookings

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@ LEARNERS = [ {
   last_name: 'Caballero',
   password: 123456754,
   email: 'faviola@mail.com',
+  information: 'Hey! I am a photographer, Filmmaker & Multimedia Artist, and I have my own Creative Agency Meta Studios in Harajuku. Before have lived in Japan and I have traveled to 97 countries looking for ancient and mystical sites around the World. I grew up in San Francisco, California',
   photo: 'https://avatars2.githubusercontent.com/u/50439580?v=4'
   },
   {
@@ -12,6 +13,7 @@ LEARNERS = [ {
   last_name: 'E. Silva Jacquier',
   password: 123456,
   email: 'fer@mail.com',
+  information: 'Fer moved to Seattle in 2018, drawn by the music scene and great outdoors.  He travels 3 months out of the year for pleasure, including Boston for the marathon and Burning Man to feast on the artistry/creativity of 50,000 folks.  He does not own a car, stare incessantly at a  smart phone, or live on a social network site, preferring the simple life.  Andrea is a native of Italy and moved to Seattle in 1995, also drawn to the music scene and the splendor of the USA.  He was worked in tech, owned a coffee shop, and been a translator.  We share the same motto:  treat others the way we want to be treated (making our american and italian catholic school nuns proud).'
   photo: 'https://avatars0.githubusercontent.com/u/47560671?v=4'
   },
   {
@@ -19,6 +21,7 @@ LEARNERS = [ {
   last_name: 'Zhang',
   password: 1234567,
   email: 'luis@mail.com',
+  information: 'I\'m a late-30s Australian who\'s easy going, tidy and respectful of other people and their spaces. I\'ve lived in a large sharehouse in East Melbourne for over 15 years, with more than 100 different people (six at a time), so I\'m pretty familiar with shared living arrangements. I particularly enjoy living with people from overseas, even if only briefly, as I find they tend to bring a cultural diversity to the house and offer a different perspective on life. That having been said, I absolutely love Australia and think Melbourne is the best city in the world, from my subjective perspective, and I very much enjoy showing people around. Some of my interests are philosophy, history and economics while I also love movies and music along with swimming and snowboarding.'
   photo: 'https://avatars1.githubusercontent.com/u/46462316?v=4'
   },
   {
@@ -26,6 +29,7 @@ LEARNERS = [ {
   last_name: 'Ratnajarn',
   password: 12345678,
   email: 'pin@mail.com',
+  information: 'International student from Thailand, open minded and friendly. Love veggie restaurants and cooking Thai food!'
   photo: 'https://avatars1.githubusercontent.com/u/37698593?v=4'
   },
   {
@@ -33,6 +37,7 @@ LEARNERS = [ {
   last_name: 'Escobar',
   password: 123456789,
   email: 'sole@mail.com',
+  information: 'I work as a digital designer. Love learning new sports and new skills. Animal lover as well - I have 3 dogs, 2 cats, and 4 turtles!'
   photo: 'https://avatars0.githubusercontent.com/u/48864600?v=4'
   },
 ]
@@ -42,6 +47,7 @@ INSTRUCTORS = [  {
   last_name: 'Lowe',
   password: 123456754,
   email: 'alberta@mail.com',
+  information:'Hi there! I\'m Alberta. I from Indonesia, and now live in the US. I love meeting new people and learning about different cultures. My hobbies include yoga, windsurfing, and drinking wine.'
   photo: 'https://avatars0.githubusercontent.com/u/50481949?v=4'
   },
   {
@@ -49,6 +55,7 @@ INSTRUCTORS = [  {
   last_name: 'Chinn',
   password: 123456,
   email: 'lisa@mail.com',
+  information: 'Hi there! I\'m Lisa. I from the US, and spent 8 years living in Tokyo. I\'m now traveling the world for as long as I can. I love meeting new people and learning about different cultures. My hobbies include playing/watching soccer, rock climbing, and going to new cafes and book stores.'
   photo: 'https://avatars1.githubusercontent.com/u/50211536?v=4'
   },
   {
@@ -56,6 +63,7 @@ INSTRUCTORS = [  {
   last_name: 'Dutson',
   password: 1234567,
   email: 'sam@mail.com',
+  information: 'Sam moved to Seattle in 2018, drawn by the music scene and great outdoors.  He travels 3 months out of the year for pleasure, including Boston for the marathon and Burning Man to feast on the artistry/creativity of 50,000 folks.  He does not own a car, stare incessantly at a  smart phone, or live on a social network site, preferring the simple life.  Andrea is a native of Italy and moved to Seattle in 1995, also drawn to the music scene and the splendor of the USA.  He was worked in tech, owned a coffee shop, and been a translator.  We share the same motto:  treat others the way we want to be treated (making our american and italian catholic school nuns proud).',
   photo: 'https://avatars3.githubusercontent.com/u/27783841?v=4'
   },
   {
@@ -63,6 +71,7 @@ INSTRUCTORS = [  {
   last_name: 'Iamele',
   password: 12345678,
   email: 'leo@mail.com',
+  information:'Leo moved to Seattle in 2018, drawn by the music scene and great outdoors.  He travels 3 months out of the year for pleasure, including Boston for the marathon and Burning Man to feast on the artistry/creativity of 50,000 folks.  He does not own a car, stare incessantly at a  smart phone, or live on a social network site, preferring the simple life.  Andrea is a native of Italy and moved to Seattle in 1995, also drawn to the music scene and the splendor of the USA.  He was worked in tech, owned a coffee shop, and been a translator.  We share the same motto:  treat others the way we want to be treated (making our american and italian catholic school nuns proud).',
   photo: 'https://avatars1.githubusercontent.com/u/39697541?v=4'
   },
   {
@@ -70,17 +79,18 @@ INSTRUCTORS = [  {
   last_name: 'Buffo',
   password: 123456789,
   email: 'sebas@mail.com',
-  photo: 'https://avatars0.githubusercontent.com/u/18401014?s=460&v=4'
+  photo: 'https://avatars0.githubusercontent.com/u/18401014?s=460&v=4',
+  information: 'Sebastián had been working for 5 years in the airline industry within Latin-America when he realized that the world was shifting towards tech. With this new aim in mind, he learned to code and today has deployed several platforms using RoR & React.',
   },
   {
   first_name: 'Samu',
   last_name: 'Levy',
   password: 12233445,
   email: 'samu@mail.com',
+  information: 'After working in the banking industry, I shifted to the startup world passionate by the idea of developing and coding new products. Furthermore, I\'ve also been working as a developer (RoR, JS, React, GraphQL) & enjoying from teaching newcomers.',
   photo: 'https://avatars0.githubusercontent.com/u/21371991?s=460&v=4'
   }
 ]
-
 
 SKILLS = [ {
     title: 'Learn to play hockey with me!' ,
@@ -99,7 +109,7 @@ SKILLS = [ {
     photo: 'https://images.unsplash.com/photo-1433800881591-a31e7bff3f22?ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80'
   },
   {
-    title: 'Learn to do yoga! Beginners welcome!',
+    title: 'Learn to do yoga!',
     description: 'Hi! I\'ve been doing yoga my whole life! I would love to share my skills with you! Hope to meet you soon :)',
     sport: 'yoga',
     price: 30,
@@ -107,7 +117,7 @@ SKILLS = [ {
     photo: 'https://images.unsplash.com/photo-1473022082832-5a30701d4f2c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80'
   },
   {
-    title: 'Come play basketball with me',
+    title: 'Come play basketball with me!',
     description: 'Hey there! I\'ve love playing \'ball! I\'ll turn you into the next Uncle Drew!',
     sport: 'basketball',
     price: 45,
@@ -115,7 +125,7 @@ SKILLS = [ {
     photo: 'https://images.unsplash.com/photo-1525916192735-22142b82d37a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80'
   },
   {
-    title: 'Climbing lessons',
+    title: 'Climbing lessons!',
     description: 'Hey there! I\'ve been climbing for a few years, and I\'ve really fallen in love with the sport. I would love to share my passion for climbing with you!',
     sport: 'rock climbing',
     price: 70,
@@ -182,6 +192,56 @@ BOOKINGS = [ {
     date: Date.tomorrow,
     num_of_hours: 1,
   },
+  {
+    status: 'accepted',
+    date: Date.today,
+    num_of_hours: 3,
+  },
+  {
+    status: 'pending',
+    date: Date.tomorrow,
+    num_of_hours: 2,
+  },
+  {
+    status: 'pending',
+    date: Date.today,
+    num_of_hours: 1,
+  },
+  {
+    status: 'cancelled',
+    date: Date.tomorrow,
+    num_of_hours: 1.5,
+  },
+  {
+    status: 'pending',
+    date: Date.tomorrow,
+    num_of_hours: 1,
+  },
+  {
+    status: 'accepted',
+    date: Date.today,
+    num_of_hours: 3,
+  },
+  {
+    status: 'pending',
+    date: Date.tomorrow,
+    num_of_hours: 2,
+  },
+  {
+    status: 'pending',
+    date: Date.today,
+    num_of_hours: 1,
+  },
+  {
+    status: 'cancelled',
+    date: Date.tomorrow,
+    num_of_hours: 1.5,
+  },
+  {
+    status: 'pending',
+    date: Date.tomorrow,
+    num_of_hours: 1,
+  }
 ]
 
 REVIEWS = [ {
@@ -283,6 +343,57 @@ REVIEWS = [ {
   },
   {
     title: 'Lo mejor instructor que he tuve!!',
+    content: 'Este instructor es el mejor. Tan bien informado y divertido también. Me divertí mucho y no puedo esperar a la próxima lección. Asegúrate de llegar temprano y calentar para que te diviertas más y te concentres en el contenido durante la clase. Altamente recomendable!',
+    stars: 5,
+  }
+  {
+    title: 'What a great instructor!!',
+    content: 'I learned so much in my lesson, and I had a lot of fun as well! Can\'t believe how much better I am after such a short amount of time! I highly recommend this class!!',
+    stars: 5,
+  },
+  {
+    title: 'Had lots of fun!!!',
+    content: 'This was the most fun I have had in a long time. I generally don\'t like working out, so I was a little bit worried about this lesson, but the time just flew by. I definitely recommend this class. Loads of fun!!',
+    stars: 4,
+  },
+  {
+    title: 'Great instructor, great class!!!',
+    content: 'This was my first time using this website to find an instructor, and I\'m really pleased with the entire experience. My instructor was really professional and knowledgeable and was a lot of fun as well. I\'m already looking forward to the next class!!',
+    stars: 5,
+  },
+  {
+    title: 'Highly recommend this instructor!!!',
+    content: 'Learned so so much in just one session! I highly recommend!!',
+    stars: 4,
+  },
+  {
+    title: 'Wonderful instructor!!! The best!',
+    content: 'This instructor is the best!!! So knowledgeable and funny as well. I just had so much fun, and I can\'t wait for the next lesson. Be sure to arrive early and warm-up so that you have more fun to focus on content during the class. Highly highly recommend!',
+    stars: 5,
+  },
+  {
+    title: 'Was a pretty good experience!!!',
+    content: 'I mostly enjoyed my lesson, but some things could have been better. The instructor arrived right on time even though I arrived 10 minutes early, and did not give me enough chance to warm-up. But the instructor was very knowledgeable and kind and I enjoyed the lesson overall. That\'s why I gave 3 stars!',
+    stars: 3,
+  },
+  {
+    title: 'Awesome time!!! Can\'t wait to go back!',
+    content: 'Loved, loved, loved this class. Already booked another class with the instructor next week. Be sure to book early because a lot of reservation slots are taken quickly!',
+    stars: 5,
+    booking_id: 7
+  },
+  {
+    title: 'Una clase maravilloso!!',
+    content: 'Aprendí mucho en mi lección, y también me divertí mucho! ¡No puedo creer lo mucho mejor que estoy después de tan poco tiempo! Recomiendo esta clase!!',
+    stars: 5,
+  },
+  {
+    title: 'Una experiencia hermosa!!!',
+    content: 'Esto fue lo más divertido que he tenido en mucho tiempo. Generalmente no me gusta hacer ejercicio, así que estaba un poco preocupado por esta lección, pero el tiempo pasó volando. Definitivamente recomiendo esta clase. Montones de diversión!!',
+    stars: 5,
+  },
+  {
+    title: 'Lo mejor instructor que he tuve!!!!',
     content: 'Este instructor es el mejor. Tan bien informado y divertido también. Me divertí mucho y no puedo esperar a la próxima lección. Asegúrate de llegar temprano y calentar para que te diviertas más y te concentres en el contenido durante la clase. Altamente recomendable!',
     stars: 5,
   }
