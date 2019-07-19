6f88b9920488363cb36e4df31e81d6a9e4ca1cbc
Lisa Chinn <lisamchinn@gmail.com>
2019-07-19 15:05:05 -0300

    finished booking_show page. the map is a photo for now because mapbox isnt' working

--- app/javascript/plugins/init_select2.js
+++ app/javascript/plugins/init_select2.js
@@ -2,7 +2,6 @@ import $ from 'jquery';
 import 'select2';
 
 const initSelect2 = () => {
-  console.log('test')
   $('.select2').select2({width: '100%'}); // (~ document.querySelectorAll)
 };
 // console.log(initSelect2)
