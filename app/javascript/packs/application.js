7ddc4a347f5ad03ca9ebc99563d8c6e006111d1b
Alberta Lowe <albertalowe93@gmail.com>
2019-07-19 15:20:33 -0300

    created star rating capabilities

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,7 @@
 import "bootstrap";
 import { initSelect2 } from '../plugins/init_select2';
+import { stars } from '../plugins/init_stars';
+// import stars from '../plugins/init_stars';
 
 initSelect2();
+stars();
