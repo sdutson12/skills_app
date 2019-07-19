9b275fd584c69d0e1a7df855e44b049997dd7922
Lisa Chinn <lisamchinn@gmail.com>
2019-07-19 16:42:47 -0300

    updated type js

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -4,12 +4,12 @@ const loadDynamicBannerText = () => {
   const banner = document.getElementById('banner-typed-text')
   if (banner) {
     new Typed('#banner-typed-text', {
-    strings: ["APTITUDE"],
-    typeSpeed: 150,
-    loop: false,
-    showCursor: false
+    strings: ["snowboard", "dunk", "ski", "backflip", "surf", "rock climb", "dribble", "swim", "serve" ],
+    typeSpeed: 120,
+    loop: true,
+    showCursor: false,
     })
   }
-}
+
 
 export { loadDynamicBannerText };
