1ee356825cc1d9af09f670970cebea170bfb5d04
Lisa Chinn <lisamchinn@gmail.com>
2019-07-18 23:48:40 -0300

    added the typing effect onto the main header on banner, only to the word 'aptitude'

---
+++ app/javascript/components/banner.js
@@ -0,0 +1,12 @@
+import Typed from 'typed.js';
+
+const loadDynamicBannerText = () => {
+  new Typed('#banner-typed-text', {
+    strings: ["APTITUDE"],
+    typeSpeed: 150,
+    loop: false,
+    showCursor: false
+  });
+}
+
+export { loadDynamicBannerText };
