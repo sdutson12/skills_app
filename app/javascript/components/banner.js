0f6711a435b67659c6122bbd1ba58a68923b61a8
Lisa Chinn <lisamchinn@gmail.com>
2019-07-19 17:05:38 -0300

    updated typed js to have more words and to go faster

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,15 +1,17 @@
 import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
-  const banner = document.getElementById('banner-typed-text')
+  const banner = document.getElementById('banner-typed-text');
+
   if (banner) {
     new Typed('#banner-typed-text', {
-    strings: ["snowboard", "dunk", "ski", "backflip", "surf", "rock climb", "dribble", "swim", "serve" ],
+    strings: ["to snowboard", "to dunk", "to ski", "to backflip", "to surf", "to do yoga", "to dribble", "to swim", "to serve", "to rock climb"],
     typeSpeed: 120,
-    loop: true,
+    loop: false,
     showCursor: false,
+    startDelay: 1000
     })
   }
-
+}
 
 export { loadDynamicBannerText };
